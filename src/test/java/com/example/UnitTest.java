667d2a39bc2d6ea81be79e42b5b19ca7656de405
Mark Woods <markwoods1977@googlemail.com>
2018-04-29 17:42:06 +0100

    Moved all test tasks to verification - added simple unit test - removed integration test from test folder

--- src/test/java/com/example/UnitTest.java
+++ src/test/java/com/example/UnitTest.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class UnitTest {
 
     @Test
     public void testUnitTest() throws Exception {
 
-
+        assertTrue(true);
     }
 }
